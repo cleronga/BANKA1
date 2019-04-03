42a9c653ea0fb459aee17a9db38263e035af67f6
NGABONZIZA Roger Clement <roclena@gmail.com>
2019-04-03 16:55:37 +0200

    sig in api endpoint

--- ApiEndpoint/bankadata.js
+++ ApiEndpoint/bankadata.js
@@ -1,10 +1,11 @@
-const user=[
-id:1,
-email:"mnre@ymail.com",
-firstname: "Roger",
-lastName: "Clement",
-password: "Ishimwe@1987",
-type: "client",
-isAdmin:"false"
+export const user= [
+{
+  id:1,
+  email:"mnre@ymail.com",
+  firstName: "Roger",
+  lastName: "Clement",
+  password: "Ishimwe@1987",
+  ctype: "client",
+  isAdmin:"false",
+}
 ]
-export default user;
